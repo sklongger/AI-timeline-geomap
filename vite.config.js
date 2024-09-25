d8f4a474ef2f84a4ca71b3c7b40176acf4d1968c
13261539115@163.com <13261539115@163.com>
2024-09-26 01:18:01 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+console.log(process.env.NODE_ENV)
+const base = process.env.NODE_ENV === 'production' ? '/AI-timeline-geomap/' : '/'
 export default defineConfig({
+  base: base,
   build: {
     sourcemap: true
   },
