a51b2f57ed3e1ce192426db5dcc2910a88112a61
13261539115@163.com <13261539115@163.com>
2024-09-27 16:24:08 +0800

    add google analytics

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from '@/App.vue'
 import 'ant-design-vue/dist/reset.css'
 import '@/style.css'
+import VueGtag from "vue-gtag";
+
 import {TimelineFlag, TimelineDrag} from '@/components/timeline/directives.ts'
 import store from '@/store/store.ts';
 import router from '@/router/router.ts';
@@ -12,5 +14,8 @@ app.directive('TimelineDrag', TimelineDrag)
 
 app.use(router)
 app.use(store)
+app.use(VueGtag, {
+    config: { id: "G-681L29M480" }
+  })
 app.mount('#app');
 
